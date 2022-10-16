d8ac98536ffc6fd69aed3f1482f42ee882637607
Mpho Kekana <mphokekana736@gmail.com>
2022-10-15 17:26:39 -0700

    functions1.c

--- functions1.c
+++ functions1.c
@@ -15,12 +15,12 @@ int print_unsigned(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned lon int);
+	unsigned long int num = va_arg(types, unsigned long int);
 
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-	buffer[i--] = '0';
+		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
@@ -67,7 +67,7 @@ int print_octal(va_list types, char buffer[],
 	}
 
 	if (flags & F_HASH && init_num != 0)
-		buffer[I--] = '0';
+		buffer[i--] = '0';
 	i++;
 
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
@@ -121,6 +121,7 @@ int print_hexa_upper(va_list types, char buffer[],
  * Return: Number of chars printed
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
+		int flags, char types, char map_to[], char buffer[],
 		int flags, char flag_ch,  int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
