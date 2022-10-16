9d33b85ffeca38d2a76ea22326c3f56568a03519
Mpho Kekana <mphokekana736@gmail.com>
2022-10-16 09:48:14 -0700

    _printf.c

--- main.h
+++ main.h
@@ -92,7 +92,7 @@ int print_rot13string(va_list types, char buffer[],
 /* width handler */
 int handle_write_char(char c, char buffer[],
 		int flags, int width, int precision, int size);
-int write_number(int is_posetive, int ind, char buffer[],
+int write_number(int is_positive, int ind, char buffer[],
 		int flags, int width, int precision, int size);
 int write_num(int ind, char bff[], int flags, int width, int precision,
 		int length, char padd, char extra_c);
