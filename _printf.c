58ab201535ec1bc8a1a55bcef340f2e318f2d657
Mpho Kekana <mphokekana736@gmail.com>
2022-10-18 16:48:03 -0700

    Group

--- _printf.c
+++ _printf.c
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -61,3 +61,6 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
